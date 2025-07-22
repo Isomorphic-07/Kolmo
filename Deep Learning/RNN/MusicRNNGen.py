ec963d90455f4f29c715163e91ab6f2b88f435dd
kieran tran <kieran.tran@student.uq.edu.au>
2025-07-22 23:18:29 +1000

    added prediction of untrained data via LSTM and embedding layers

--- Deep Learning/RNN/MusicRNNGen.py
+++ Deep Learning/RNN/MusicRNNGen.py
@@ -26,8 +26,8 @@ songs = mdl.lab1.load_training_data()
 
 #Print one of the songs to inspect
 example_song = songs[0]
-print("\nExample song: ")
-print(example_song)
+#print("\nExample song: ")
+#print(example_song)
 # Convert the ABC notation to audio file and listen to it
 mdl.lab1.play_song(example_song)
 
@@ -44,7 +44,7 @@ songs_joined = "\n\n".join(songs)
 
 # Find all unique characters in the joined string
 vocab = sorted(set(songs_joined))
-print("There are", len(vocab), "unique characters in the dataset")
+#print("There are", len(vocab), "unique characters in the dataset")
 
 """
 We want to train an RNN model to learn patterns in ABC music, and then use this model to
@@ -99,7 +99,7 @@ def vectorize_string(string):
 
 vectorized_songs = vectorize_string(songs_joined)
 
-print ('{} ---- characters mapped to int ----> {}'.format(repr(songs_joined[:10]), vectorized_songs[:10]))
+# print ('{} ---- characters mapped to int ----> {}'.format(repr(songs_joined[:10]), vectorized_songs[:10]))
 # check that vectorized_songs is a numpy array
 assert isinstance(vectorized_songs, np.ndarray), "returned result should be a numpy array"
 
@@ -132,7 +132,7 @@ def get_batch(vectorized_songs, seq_length, batch_size):
     
     #essentially the target batch, we want to predict these
     '''TODO: construct a list of output sequences for the training batch'''
-    output_batch = np.array([vectorized_songs[n + 1: n + seq_length + 1] for n in idx])
+    output_batch = np.array([vectorized_songs[n + 1: n + seq_length + 1] for n in idx]) 
     
      # Convert the input and output batches to tensors
     x_batch = torch.tensor(input_batch, dtype=torch.long)
@@ -144,7 +144,7 @@ test_args = (vectorized_songs, 10, 2)
 x_batch, y_batch = get_batch(*test_args)
 assert x_batch.shape == (2, 10), "x_batch shape is incorrect"
 assert y_batch.shape == (2, 10), "y_batch shape is incorrect"
-print("Batch function works correctly!")
+#print("Batch function works correctly!")
 #print(f"{x_batch}, {y_batch}")
 
 """
@@ -157,7 +157,152 @@ state in addition to the current input.
 
 x_batch, y_batch = get_batch(vectorized_songs, seq_length= 5, batch_size= 1)
 
+
+"""
 for i, (input_idx, target_idx) in enumerate(zip(x_batch[0], y_batch[0])):
     print("Step {:3d}".format(i))
     print("  input: {} ({:s})".format(input_idx, repr(idx2char[input_idx.item()])))
     print("  expected output: {} ({:s})".format(target_idx, repr(idx2char[target_idx.item()])))
+"""
+    
+"""
+Lets now build the RNN model, and the use the trained model to generate a new song.
+We train the RNN using batches of song snippets from the dataset. 
+
+The model is based off the LSTM architecture, where we use a state vector to maintain information
+about the temporal relationships between consecutive characters. The final output
+of the LSTM is then fed into a fully connected linear nn.Linear layer where we'll output a softmax over each
+character in the vocabulary, and then sample from this distribution to predict the next character
+
+nn.Embedding: input layer, consisting of a trainable lookup table that maps the 
+numbers of each character to a vector with embedding_dim dimensions
+nn.LSTM: LSTM network, with size hidden_size
+nn.Linear: output layer, with vocab_size outputs
+"""
+
+#Defining the RNN model
+
+class LSTMModel(nn.Module):
+    def __init__(self, vocab_size, embedding_dim, hidden_size):
+        super(LSTMModel, self).__init__()
+        
+        self.hidden_size = hidden_size
+        
+        #define each network layer, layer 1 is the embedding layer to transform indices 
+        #into dense vectors of a fixed embedding size
+        # inputs are vectors of integer indices, the embedding layer maps each integer
+        # to a real-valued vector of dimension embedding_dim
+        # essentially, the embedding layer converts high-dimensional data into
+        # a lower-dimensional space. This helps models to understand and work with complex
+        # data more efficiently
+        self.embedding = nn.Embedding(vocab_size, embedding_dim)
+        
+        '''TODO: Layer 2: LSTM with hidden_size `hidden_size`. note: number of layers defaults to 1.
+         Use the nn.LSTM() module from pytorch.'''
+        #LSTM: long short term memory is an RNN architecture designed to address vanishing gradient problem
+        self.lstm = nn.LSTM(input_size= embedding_dim, hidden_size= self.hidden_size, batch_first= True) # TODO
+        #really important to do, haha went on a full dimensional analysis of everything
+        # do batch_first so that the LSTM knows which shape value is which in construction of RNN
+        
+        '''TODO: Layer 3: Linear (fully-connected) layer that transforms the LSTM output
+        #   into the vocabulary size.'''
+        self.fc = nn.Linear(in_features= self.hidden_size, out_features= vocab_size) # TODO
+    
+    def init_hidden(self, batch_size, device):
+        #initialize the hidden state and cell state with zeros:
+        #this is needed as nn.LSTM expects an initial state at each forward pass
+        #cell states are a vital part of LSTMs, as they act like long-term memory
+        return (torch.zeros(1, batch_size, self.hidden_size).to(device),
+                torch.zeros(1, batch_size, self.hidden_size).to(device))
+        #1: one layer, one direction
+        #batch_size: number of sequences in the batch
+        #self.hidden_state = number of features in hidden state
+        
+        #.to(device) ensures both h_0 ad c_0 are on the same device as the model/input
+        
+    def forward(self, x, state = None, return_state = False):
+        x = self.embedding(x) #we pass a batch of token indices to convert to a dense vector
+        #x.shape: (12, 100) -> (12, 100, embedding_dim)
+        print(x.size())
+        
+        if state is None:
+            state = self.init_hidden(x.size(0), x.device)
+            print(x.size(0))
+        out, state = self.lstm(x, state)
+        #Note (refer to PyTorch docstrings)
+        #LSTM takes in as input: x of shape (batch_size, seq_length, embedding_dim)
+        # also takes in the state (h_0, c_0)
+        # it will output out: shape (batch_size, seq_length, hidden_size) -> sequence of outputs for all time steps
+        # and state: (tuple): (h_n, c_n)-> final hidden and cell states for continuation
+        
+        
+        out = self.fc(out)
+        #we note here that self.fc is nn.Linear(hidden_size, vocab_size)
+        # For each  LSTM output vector, it computes logits
+        # logits_t = W h_t + b \in \mathbb{R}^{vocab_size}
+        
+        return out if not return_state else (out, state)
+        
+
+#Instantiate the model:
+vocab_size = len(vocab)
+embedding_dim = 256
+hidden_size = 1024
+batch_size = 8
+#note the powers of 2
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+#checks if the GPU is available
+
+model = LSTMModel(vocab_size, embedding_dim, hidden_size).to(device)
+
+#print summary of model
+print(model)
+
+#let us now test the RNN model:
+x, y = get_batch(vectorized_songs, seq_length = 100, batch_size= 32)
+x = x.to(device)
+y = y.to(device)
+
+pred = model(x) #input x (shape (12, 10)) is passed through the Embedding layer
+# -> shape (12, 100, 256), then into the LSTM -> output shape (12, 100, 1024)
+# -> then through linear layer -> pred.shape (12, 100, vocab_size)
+# the thing to note here is batch_size = 8 is just a default hyperparameter and so the model doesn't
+# care too much about batch size- it generalizes to any batch size. 
+print("Input shape:      ", x.shape, " # (batch_size, sequence_length)")
+print("Prediction shape: ", pred.shape, "# (batch_size, sequence_length, vocab_size)")
+
+#lets get predictions from the untrained model (to train it we need to define a loss function)
+"""
+To get predictions from the model, we sample from the output distribution, defined by a softmax
+over the character vocabulary. This gives us actual character indices, meaning we are
+using a categorical distribution to sample over the example prediction. This gives
+us a prediction of the next character specifically its index at each timestep. 
+
+torch.multinomial samples over a categorical distribution to generate predictions
+Note here that we sample from this probability distribution rather than just taking the
+argmax which introduce stochasticity into the generation
+"""
+
+sampled_indices = torch.multinomial(torch.softmax(pred[0], dim = -1), num_samples = 1)
+#note here that pred[0].shape = (seq_length, vocab_size), meaning that for 
+# each of the seq_length time steps, we have a vector of logits over the vocabulary
+# logits are essentially unnormalised output like z. 
+# softmax then converts the logits into probabilities where each row sums to 1 to
+#ensure a valid categorical distribution. 
+# torch.multinomial now samples one index per timestep from the distribution,
+# each row (each timestep), multinomial samples one index from the vocab according to categorical
+# probabiltiies
+#sampled_indices.shape = (seq_length, 1) where each row is now th epredicted character
+# index at that time step. 
+sampled_indices = sampled_indices.squeeze(-1).cpu().numpy()
+#this squeezes out the last dimension, so now its a 1D array of character indices
+#sampled_indices.shape = (seq_length,)
+#cpu().numpy(), if we were on a GPU, this detaches the tensor and moves it to 
+# CPU for further processing and converts it to a NumPy array. 
+
+
+#lets now decode this:
+print("Input: \n", repr("".join(idx2char[x[0].cpu()])))
+print()
+print("Next Char Predictions: \n", repr("".join(idx2char[sampled_indices])))
